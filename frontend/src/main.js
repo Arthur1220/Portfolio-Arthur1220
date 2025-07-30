9c40add2205a8959bc8bc47990d036e7683d009d
Arthur Marques Azevedo <arthurmarquesazevedo@gmail.com>
2025-07-30 18:23:41 -0300

    refactor: reorganizar importações e simplificar a inicialização da aplicação

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,24 +1,29 @@
-import './assets/main.css'
-import 'devicon/devicon.min.css'
-import i18n from './i18n'
-
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
 
+// Importações de CSS
+import './assets/main.css'
+import 'devicon/devicon.min.css'
+import 'vue3-carousel/dist/carousel.css'
+import 'aos/dist/aos.css';
+
+// Importação do i18n e AOS
+import i18n from './i18n'
+import AOS from 'aos';
+
+// Criação da aplicação
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
-app.use(router)
+// Uso dos plugins
+app.use(pinia)
 app.use(i18n)
 
+// Montagem da aplicação
 app.mount('#app')
 
+// Inicialização do AOS
 AOS.init({
   duration: 800,
   once: true,
